85221977cf8b989267959a27886f8d377e74e1be
cristhian2121 <ccdelgadop@gmail.com>
2021-02-07 21:28:20 -0500

    Add tailwindcss in proyect

--- pages/hero/index.tsx
+++ pages/hero/index.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect, useMemo, } from 'react';
 import { SuperHeroService } from '../../services/superHero/superHeroService';
 
+// tailwindcss styles
+import "tailwindcss/tailwind.css";
+// import "styles/global.css"
+
 const HeroesList = ({ }) => {
 
     const [heroes, setheroes] = useState([]);
@@ -12,16 +16,24 @@ const HeroesList = ({ }) => {
     async function getHeroes() {
         const superHeroService = new SuperHeroService();
         const { state, data } = await superHeroService.getAll()
-        if(state && data.data && data.data.result){
+        if (state && data.data && data.data.result) {
             setheroes(data.data.result);
         }
     }
 
     return (
         <div className="">
-            
+            <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+                <div className="flex-shrink-0">
+                    <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
+                </div>
+                <div>
+                    <div className="text-xl font-medium text-black">ChitChat</div>
+                    <p className="text-gray-500">You have a new message!</p>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default HeroList
+export default HeroesList;
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+// tailwind.config.js
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  variants: {
+    extend: {
+      borderColor: ['focus-visible'],
+      opacity: ['disabled'],
+    }
+  }
+}
