6bd6ab3b76b9b2a9367a640cba625d0830b6f883
cristhian2121 <ccdelgadop@gmail.com>
2021-02-09 21:53:04 -0500

    create document extend

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+    // When you need something from server
+//   static async getInitialProps(ctx) {
+//     const initialProps = await Document.getInitialProps(ctx)
+//     return { ...initialProps }
+//   }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+            {/* Styles extern */}
+            {/* scripts */}
+            {/* faviicon */}
+        </Head>
+        <body className="my-body-class">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
